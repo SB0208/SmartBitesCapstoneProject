45dd73d7f45b171529ffd3818136be7dd15ba103
SB0208 <baevasnezhina@gmail.com>
2024-10-17 14:22:29 +0200

    update backend

--- backend/src/main/java/com/library/backend/controller/UserController.java
+++ backend/src/main/java/com/library/backend/controller/UserController.java
@@ -1,16 +1,12 @@
 package com.library.backend.controller;
+
 import com.library.backend.model.User;
 import com.library.backend.service.UserService;
-import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/users")
-
-
-
 public class UserController {
 
     private final UserService userService;
@@ -19,7 +15,6 @@ public class UserController {
     public UserController(UserService userService) {
         this.userService = userService;
     }
-
 
     @PostMapping("/register")
     public User register(@RequestBody User user) {
@@ -30,6 +25,5 @@ public class UserController {
     public User login(@RequestParam String username, @RequestParam String password) {
         return userService.loginUser(username, password);
     }
-
-
 }
+
