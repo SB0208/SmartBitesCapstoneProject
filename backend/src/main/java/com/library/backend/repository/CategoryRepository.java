85d4d2cfca7f05a025e37053eff9a68512b3aca1
SB0208 <baevasnezhina@gmail.com>
2024-10-30 10:44:55 +0100

    backend update

--- backend/src/main/java/com/library/backend/repository/CategoryRepository.java
+++
@@ -1,8 +0,0 @@
-package com.library.backend.repository;
-import com.library.backend.model.Category;
-import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.stereotype.Repository;
-@Repository
-
-public interface CategoryRepository extends MongoRepository<Category, String> {
-}
