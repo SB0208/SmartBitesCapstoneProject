0e4d4d89ab758de56fa3e683cb0b89148c41f5fb
SB0208 <baevasnezhina@gmail.com>
2024-11-08 12:25:15 +0100

    test

---
+++ backend/src/test/java/com/library/backend/model/MealTest.java
@@ -0,0 +1,95 @@
+package com.library.backend.model;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class MealTest {
+
+    @Test
+    void getId() {
+    }
+
+    @Test
+    void setId() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void setName() {
+    }
+
+    @Test
+    void getDescription() {
+    }
+
+    @Test
+    void setDescription() {
+    }
+
+    @Test
+    void getCategory() {
+    }
+
+    @Test
+    void setCategory() {
+    }
+
+    @Test
+    void getType() {
+    }
+
+    @Test
+    void setType() {
+    }
+
+    @Test
+    void getLink() {
+    }
+
+    @Test
+    void setLink() {
+    }
+
+    @Test
+    void getIngredients() {
+    }
+
+    @Test
+    void setIngredients() {
+    }
+
+    @Test
+    void getNutrition() {
+    }
+
+    @Test
+    void setNutrition() {
+    }
+
+    @Test
+    void getHealthBenefit() {
+    }
+
+    @Test
+    void setHealthBenefit() {
+    }
+
+    @Test
+    void testEquals() {
+    }
+
+    @Test
+    void canEqual() {
+    }
+
+    @Test
+    void testHashCode() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
