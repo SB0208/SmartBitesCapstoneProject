99f4a246013c13f8da5e732e029550ccec1b65df
SB0208 <baevasnezhina@gmail.com>
2024-11-08 12:30:12 +0100

    test

---
+++ backend/src/test/java/com/library/backend/controller/MealControllerTest.java
@@ -0,0 +1,44 @@
+package com.library.backend.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MealControllerTest {
+
+    @Test
+    void getAllMeals() {
+    }
+
+    @Test
+    void getMealById() {
+    }
+
+    @Test
+    void getMealsByCategory() {
+    }
+
+    @Test
+    void getMealsByType() {
+    }
+
+    @Test
+    void getMealsByCategoryAndType() {
+    }
+
+    @Test
+    void testGetMealsByCategory() {
+    }
+
+    @Test
+    void createMeal() {
+    }
+
+    @Test
+    void updateMeal() {
+    }
+
+    @Test
+    void deleteMeal() {
+    }
+}
