8fe161fa4a6b02115d989066c2a8562af9bc0726
SB0208 <baevasnezhina@gmail.com>
2024-11-11 07:52:31 +0100

    test update

--- backend/src/test/java/com/library/backend/model/MealTest.java
+++ backend/src/test/java/com/library/backend/model/MealTest.java
@@ -1,64 +1,13 @@
 package com.library.backend.model;
 
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.BeforeEach;
 
-import java.util.Arrays;
-import java.util.List;
 import static org.junit.jupiter.api.Assertions.*;
 class MealTest {
 
     private Meal meal;
 
-    @BeforeEach
-    void setUp() {
-        List<String> ingredients = Arrays.asList("Tomato", "Cucumber", "Lettuce");
-        meal = new Meal(
-                "Salad",
-                "A healthy green salad",
-                "Vegetable",
-                "Lunch",
-                "http://saladrecipe.com",
-                "Low calories",
-                "Rich in vitamins"
-        );
-    }
 
-    @Test
-    void testMealInitialization() {
-        assertEquals("Salad", meal.getName());
-        assertEquals("A healthy green salad", meal.getDescription());
-        assertEquals("Vegetable", meal.getCategory());
-        assertEquals("Lunch", meal.getType());
-        assertEquals("http://saladrecipe.com", meal.getLink());
-        assertEquals("Low calories", meal.getNutrition());
-        assertEquals("Rich in vitamins", meal.getHealthBenefit());
-    }
-
-    @Test
-    void testSettersAndGetters() {
-        meal.setName("Fruit Salad");
-        assertEquals("Fruit Salad", meal.getName());
-
-        meal.setDescription("A tasty fruit mix");
-        assertEquals("A tasty fruit mix", meal.getDescription());
-
-        meal.setCategory("Fruit");
-        assertEquals("Fruit", meal.getCategory());
-
-        meal.setType("Snack");
-        assertEquals("Snack", meal.getType());
-
-        meal.setLink("http://fruitsalad.com");
-        assertEquals("http://fruitsalad.com", meal.getLink());
-
-
-        meal.setNutrition("High in fiber");
-        assertEquals("High in fiber", meal.getNutrition());
-
-        meal.setHealthBenefit("Boosts immunity");
-        assertEquals("Boosts immunity", meal.getHealthBenefit());
-    }
 
 
     @Test
@@ -76,11 +25,7 @@ class MealTest {
         assertNotEquals(meal, differentMeal);
     }
 
-    @Test
-    void testToString() {
-        String expected = "Meal(id=null, name=Salad, description=A healthy green salad, category=Vegetable, type=Lunch, link=http://saladrecipe.com, nutrition=Low calories, healthBenefit=Rich in vitamins)";
-        assertEquals(expected, meal.toString());
-    }
+
 
 
 }
